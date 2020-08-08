0097c994dd355525d0cddf0bc608fa0e122cc627
ruchikamehra <mehrasurendra02@gmail.com>
2020-08-09 02:19:01 +0530

    second commit

---
+++ Cyber_ques_3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,j,c;
+printf("enter a and b");
+scanf("%d%d",&a,&b);
+for(i=a;i<=b;i++)
+{
+c=0;
+for(j=1;j<=i;j++)
+{
+if(i%j==0)
+{
+c++;
+}
+}
+if(c==2)
+{
+printf("%d\n",i);
+}
+}
+return 0;
+}
