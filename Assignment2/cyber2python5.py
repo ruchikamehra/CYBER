c55402d547fa8e92a9565cd974b5d6908eed5fd2
ruchikamehra <mehrasurendra02@gmail.com>
2020-09-01 14:12:11 +0530

    fifth commit

---
+++ Assignment2/cyber2python5.py
@@ -0,0 +1,13 @@
+def multipylis(list):
+    mul=1
+    for i in range(1,l):
+          mul=mul*lis[i]
+    return(mul)
+l1=int(input("enter the elements in the list"))
+lis=[]
+for i in range(0,l1):
+    x=int(input())
+    lis.append(x)
+l=len(lis)
+print(lis)
+print("the multiply is",multipylis(lis))
